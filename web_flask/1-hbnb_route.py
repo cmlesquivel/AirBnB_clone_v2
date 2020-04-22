3de1c5c5a0b2bbc66576bcf866e74d54d66271f8
cmlesquivel <1210@holbertonschool.com>
2020-04-22 17:59:43 +0000

    add the route /hbnb to the app

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" script that starts a Flask"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def print_hello():
+    """ Show into the screen Hello HBNB!"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def print_hello():
+    """ Show into the screen HBNB!"""
+    return ("HBNB")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
